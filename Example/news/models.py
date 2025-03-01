af9de1cc35de76b00fb7a4817f7e43a49204ebb5
24-python <24_python@mail.ru>
2025-03-01 17:48:57 +0700

    1

--- Example/news/models.py
+++ Example/news/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class News_post(models.Model):
+    title = models.CharField('Название новости', max_length=50)
+    short_description = models.CharField('Короткая новость', max_length=200)
+    text = models.TextField('Новость')
+    pub_date = models.DateTimeField('Дата публикации')
+
+    class Meta:
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'

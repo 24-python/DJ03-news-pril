90e26daed3ac82768ad00bdd5405a3b88c86ab9b
24-python <24_python@mail.ru>
2025-03-02 20:08:51 +0300

    Готовое ДЗ

--- Example/news/admin.py
+++ Example/news/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import News_post
+from .models import News_post, Full_post
+
 # Register your models here.
 
 admin.site.register(News_post)
+admin.site.register(Full_post)

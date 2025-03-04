207d33f8499d25e3d8b82d766c190eec3f1de27c
24-python <24_python@mail.ru>
2025-03-04 20:16:45 +0300

    2

--- Example/news/views.py
+++ Example/news/views.py
@@ -5,5 +5,8 @@ def home(request):
 	news = Full_post.objects.all()
 	return render(request, 'news/news.html', {'news': news})
 
+def create_news(request):
+	return render(request, 'news/add_new_post.html')
+
 
 
